1af09845128136989e9d7254a4a72da26faf2d9f
Joshua Howland <development@jkhowland.com>
2014-05-12 22:59:34 -0600

    Initial Commit

---
+++ GitReference/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  GitReference
+//
+//  Created by Joshua Howland on 5/12/14.
+//  Copyright (c) 2014 DevMountain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "GRAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([GRAppDelegate class]));
+    }
+}
