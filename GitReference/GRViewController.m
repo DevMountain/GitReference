22594f5e14f28b4738f8d3522a8299eea169708a
Joshua Howland <development@jkhowland.com>
2014-05-13 00:41:50 -0600

    Adds commands and references to the view

--- GitReference/GRViewController.m
+++ GitReference/GRViewController.m
@@ -8,7 +8,9 @@
 
 #import "GRViewController.h"
 
-static CGFloat margin = 10;
+static CGFloat margin = 15;
+static NSString * const Command = @"command";
+static NSString * const Reference = @"reference";
 
 @interface GRViewController ()
 
@@ -32,7 +34,7 @@ static CGFloat margin = 10;
     // Note: In this case, view.bounds is the same as view.frame. However, oftentimes the bounds will have an origin that is (0,0) whereas the frame will not.
     
     UIScrollView *scrollView = [[UIScrollView alloc] initWithFrame:self.view.bounds];
-    scrollView.contentSize = CGSizeMake(self.view.frame.size.width, self.view.frame.size.height * 2);
+    scrollView.contentSize = CGSizeMake(self.view.frame.size.width, self.view.frame.size.height * 1.75);
     [self.view addSubview:scrollView];
     
     CGFloat topMargin = 40;
@@ -42,6 +44,40 @@ static CGFloat margin = 10;
     title.text = @"GitReference";
     [scrollView addSubview:title];
     
+    CGFloat top = topMargin + 20 + margin * 2;
+    
+    for (int i = 0; i < [[self gitCommands] count]; i++) {
+        
+        UILabel *gitCommand = [[UILabel alloc] initWithFrame:CGRectMake(margin, top, self.view.frame.size.width - 2 * margin, 20)];
+        gitCommand.font = [UIFont boldSystemFontOfSize:17];
+        gitCommand.text = [self gitCommands][i][Command];
+        [scrollView addSubview:gitCommand];
+        
+        top += (20 + margin);
+        
+        CGFloat heightForReference = 41;
+        
+        UILabel *gitReference = [[UILabel alloc] initWithFrame:CGRectMake(margin, top, self.view.frame.size.width - 2 * margin, heightForReference)];
+        gitReference.numberOfLines = 0;
+        gitReference.font = [UIFont systemFontOfSize:15];
+        gitReference.text = [self gitCommands][i][Reference];
+        [scrollView addSubview:gitReference];
+        
+        top += (heightForReference + margin * 2);
+    }
+}
+
+- (NSArray *)gitCommands {
+
+    return @[@{Command: @"git status", Reference: @": shows changed files"},
+             @{Command: @"git diff", Reference: @": shows actual changes"},
+             @{Command: @"git add .", Reference: @": adds changed files to the commit"},
+             @{Command: @"git commit -m \"notes\"", Reference: @": commits the changes"},
+             @{Command: @"git push origin _branch_", Reference: @": pushes commits to branch named _branch_"},
+             @{Command: @"git log", Reference: @": displays progress log"},
+             @{Command: @"git comment --amend", Reference: @": re-enter last commit message"}
+             ];
+
 }
 
 @end
